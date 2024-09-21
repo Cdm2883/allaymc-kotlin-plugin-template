63fb6dd1149191247873ebb069171185429e6560
Cdm2883 <Cdm2883@hotmail.com>
2024-09-22 02:04:44 +0800

    build: rename api module

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(group = "org.allaymc.allay", name = "Allay-API", version = "master-SNAPSHOT")
+    compileOnly(group = "org.allaymc.allay", name = "api", version = "master-SNAPSHOT")
 }
 
 kotlin {
