604de2f6d378ac6f449f8c9d6009e2733d897c31
Cdm2883 <Cdm2883@hotmail.com>
2024-09-22 13:58:44 +0800

    feat: new plugin logger

--- src/main/kotlin/vip/cdms/allayplugin/KotlinPluginTemplate.kt
+++ src/main/kotlin/vip/cdms/allayplugin/KotlinPluginTemplate.kt
@@ -1,24 +1,10 @@
 package vip.cdms.allayplugin
 
 import org.allaymc.api.plugin.Plugin
-import org.slf4j.LoggerFactory
 
 @Suppress("unused")
 class KotlinPluginTemplate : Plugin() {
-    private val log = LoggerFactory.getLogger(this::class.java)
-
-    override fun onLoad() {
-        super.onLoad()
-        log.info("KotlinPluginTemplate loaded!")
-    }
-
-    override fun onEnable() {
-        super.onEnable()
-        log.info("KotlinPluginTemplate enabled!")
-    }
-
-    override fun onDisable() {
-        super.onDisable()
-        log.info("KotlinPluginTemplate disabled!")
-    }
+    override fun onLoad() = pluginLogger.info("KotlinPluginTemplate loaded!")
+    override fun onEnable() = pluginLogger.info("KotlinPluginTemplate enabled!")
+    override fun onDisable() = pluginLogger.info("KotlinPluginTemplate disabled!")
 }
